ecfaf37e3a1180d7d3f9a6b5aacb11a4c1ddb2bc
Eric Brito <eric_brito@msn.com>
2020-11-21 15:44:03 -0300

    - Adicionada extensão para UIAlertController

--- Sources/AfrodevUtils/AfrodevUtils.swift
+++
@@ -1,3 +0,0 @@
-struct AfrodevUtils {
-    var text = "Hello, World!"
-}
---
+++ Sources/AfrodevUtils/UIAlertController+Show.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIAlertController {
+    static func showAlert(presenter: UIViewController, title: String, message: String, okActionHandler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: okActionHandler)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        presenter.present(alert, animated: true, completion: nil)
+    }
+}
